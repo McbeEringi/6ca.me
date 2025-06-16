c66b5d3bfcd300a3443b1cbfc93798f9b035b891
McbeEringi <git@mcbeeringi.dev>
2025-06-17 02:55:50 +0900

    -

--- functions/[name].mjs
+++ functions/[name].mjs
@@ -1,8 +1,12 @@
 const
-rand=_=>(w=>(w=[...w(10,0x30),...w(26,0x41),...w(26,0x61),...'-_'],[...Array(10)].map(_=>w[Math.random()*64|0]).join('')))((l,o)=>[...Array(l)].map((_,i)=>String.fromCharCode(i+o)))
+rand=_=>(w=>(
+	w=[...w(10,0x30),...w(26,0x41),...w(26,0x61),...'-_'],
+	[...Array(10)].map(_=>w[Math.random()*64|0]).join('')
+))((l,o)=>[...Array(l)].map((_,i)=>String.fromCharCode(i+o))),
 links={
 	// QRCode v1: 10 chars
 	// chars from base64url [A-Za-z0-9\-_]
+	// use rand()
 	'_1dKisX0qg':{desc:'test',href:'https://github.com/mcbeeringi'}
 },
 onRequest=ctx=>new Response(...({
