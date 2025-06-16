0e50f3075a65c4003df284ed09eb6fe2e64792ee
McbeEringi <git@mcbeeringi.dev>
2025-06-17 02:22:00 +0900

    -

---
+++ functions/[name].mjs
@@ -0,0 +1,6 @@
+const
+onRequest=ctx=>new Response(...({
+	links:_=>[JSON.stringfy('link table goes here...'),{headers:{'Content-Type':'application/json'}}]
+}[ctx.params.name]||_=>`Hello, ${ctx.params.name}!`)())
+
+export{onRequest};
--- functions/test/[name].mjs
+++
@@ -1,4 +0,0 @@
-const
-onRequest=ctx=>ctx.params.name.length<4&&new Response(`Hello, ${ctx.params.name}!`)
-
-export{onRequest};
