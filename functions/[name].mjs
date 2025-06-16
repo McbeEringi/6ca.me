2f737b91b0976975839dad26aaed7f6a87906c4f
McbeEringi <git@mcbeeringi.dev>
2025-06-17 02:54:43 +0900

    -

--- functions/[name].mjs
+++ functions/[name].mjs
@@ -1,6 +1,15 @@
 const
+rand=_=>(w=>(w=[...w(10,0x30),...w(26,0x41),...w(26,0x61),...'-_'],[...Array(10)].map(_=>w[Math.random()*64|0]).join('')))((l,o)=>[...Array(l)].map((_,i)=>String.fromCharCode(i+o)))
+links={
+	// QRCode v1: 10 chars
+	// chars from base64url [A-Za-z0-9\-_]
+	'_1dKisX0qg':{desc:'test',href:'https://github.com/mcbeeringi'}
+},
 onRequest=ctx=>new Response(...({
-	links:_=>[JSON.stringfy('link table goes here...'),{headers:{'Content-Type':'application/json'}}]
-}[ctx.params.name]||(_=>`Hello, ${ctx.params.name}!`))());
+	links:_=>[JSON.stringify(links),{headers:{'Content-Type':'application/json'}}]
+}[ctx.params.name]||(i=>(
+	links[i]?[JSON.stringify(links[i]),{status:301,headers:{Location:links[i].href}}]:
+	`Hello, ${ctx.params.name}!`
+)))(ctx.params.name));
 
 export{onRequest};
