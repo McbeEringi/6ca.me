1d0fe6869e1d32ef5763a85729d0bc79add2b34d
McbeEringi <git@mcbeeringi.dev>
2025-06-25 07:57:56 +0900

    -

--- src/index.mjs
+++ src/index.mjs
@@ -16,11 +16,15 @@ links={
 	q:{description:'Qiita',Location:'https://qiita.com/mcbeeringi'},
 
 	'_1dKisX0qg':{description:'test',Location:'https://github.com/mcbeeringi'}
-},
-onRequest=ctx=>({
-	links:_=>new Response(JSON.stringify(links),{headers:{'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}})
-}[ctx.params.name]||(i=>(
-	links[i]?new Response(null,{status:301,headers:links[i]}):ctx.next()
-)))(ctx.params.name);
+};
 
-export default{fetch:onRequest};
+export default{
+	fetch:req=>(
+		req.url=new URL(req.url),
+		({
+			links:_=>new Response(JSON.stringify(links),{headers:{'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}})
+		}[req.url.pathname]||(i=>(
+			links[i]?new Response(null,{status:301,headers:links[i]}):new Response(null,{status:404})
+		)))(req.url.pathname)
+	)
+};
