a9000f7349b4ff62cae4053f1a9b5acbcb0b8e1a
McbeEringi <git@6ca.me>
2026-09-03 03:46:30 +0900

    -

--- build.mjs
+++ build.mjs
@@ -1,68 +1,68 @@
 #!/usr/bin/env -S bun --install=force
 import{join}from'node:path';
-import vm from'node:vm';
+import VM from'node:vm';
 
 const
 src='./src',
-config_default={
-	title:'Hello world!',
-	desc:'description goes here...',
-	favicon:'/img/favicon.png',
-	ogpimg:'/img/icon.png',
-};
-
-// console.log(
-
-await Promise.all(
-	new Bun.Glob('**/*.html').scanSync(src)[Symbol.iterator]().map(async x=>(
-		x={path:x},
-		Object.assign(x,await(async(config='',config_mime,bun='')=>({
-			html:new HTMLRewriter()
-				.on(
-					`
-					script#config[type="application/toml"],
-					script#config[type="application/yaml"],
-					script#config[type="application/json"],
-					script#config[type="application/json5"]
-					`,
-					{
-						element:e=>(
-							config_mime=e.getAttribute('type').replace(/^application\//,'').toUpperCase(),
-							e.remove()
-						),
-						text:x=>config+=x.text
-					}
-				)
-				.on(`script#bun`,{element:e=>e.remove(),text:x=>bun+=x.text})
-				.transform(await Bun.file(join(src,x.path)).text()),
-			config_mime,
-			config:Object.assign({},config_default,(Bun[config_mime]??self[config_mime]).parse(config)),
-			bun:(
-				bun=new vm.SourceTextModule(bun),
-				await bun.link(_=>_),
-				await bun.evaluate(),
-				bun.namespace.default
-			)
-		}))()),
-		x.html=`\
+dst='./dst',
+modules=Object.entries({
+	config_default:{
+		title:'Hello world!',
+		desc:'description goes here...',
+		favicon:'/img/favicon.png',
+		ogpimg:'/img/icon.png',
+		origin:'https://6ca.me/'
+	},
+	html_tmpl:c=>x=>((
+		url=new URL(x.path,c.origin).href
+	)=>({...x,html:`\
 <!DOCTYPE html>
 <html prefix="og:http://ogp.me/ns#" lang="ja"><head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<title>${x.config.title}</title>
-	<meta name="description" content="${x.config.desc}">
-	<link rel="icon" type="image/png" href="${x.config.favicon}">
+	<title>${c.title}</title>
+	<meta name="description" content="${c.desc}">
+	<link rel="icon" type="image/png" href="${c.favicon}">
 	<meta property="og:type" content="website">
-	<meta property="og:title" content="${x.config.title}">
-	<meta property="og:description" content="${x.config.desc}">
-	<meta property="og:url" content="https://l.6ca.me/${x.path}">
-	<meta property="og:image" content="https://6ca.me/${x.config.ogpimg}">
-	<link href="https://6ca.me/src/style.css" rel="stylesheet">
-</head><body>${x.html}</body></html>
-`,
-		x.bun?.(x),
-		x
-	))
-)
+	<meta property="og:title" content="${c.title}">
+	<meta property="og:description" content="${c.desc}">
+	<meta property="og:url" content="${url}">
+	<meta property="og:image" content="${new URL(c.ogpimg,url)}">
+	<link href="/src/style.css" rel="stylesheet">
+${x.style?`\t<style>${x.style}</style>\n`:''}\
+</head><body>
+${x.html}\
+</body></html>
+`
+	}))()
+}).reduce((a,[i,x])=>(
+	a[i]=new VM.SyntheticModule(['default'],_=>a[i].setExport('default',x)),
+	a
+),{});
 
-// );
+await Bun.$`rm -rf ${dst}`;
+await Bun.$`cp -R ${src}/. ${dst}`;
+
+await Promise.all(
+	new Bun.Glob('**/*.html').scanSync(src)[Symbol.iterator]().map(async x=>(
+		console.log(x),
+		x={path:x},
+		Object.assign(x,await(async(mod='',css='')=>({
+			html:new HTMLRewriter()
+				.on(`script[type=build]`,{element:e=>e.remove(),text:x=>mod+=x.text+(x.lastInTextNode?'\n':'')})
+				.on(`style[head]`,{element:e=>e.remove(),text:x=>css+=x.text+(x.lastInTextNode?'\n':'')})
+				.transform(await Bun.file(join(dst,x.path)).text()).trim()+'\n',
+			style:css.slice(0,-1),
+			build:(
+				mod=new VM.SourceTextModule(mod),
+				mod.linkRequests(mod.moduleRequests.map(x=>modules[x.specifier])),
+				mod.instantiate(),
+				await mod.evaluate(),
+				mod.namespace.default
+			)
+		}))()),
+		x=x.build?.(x)??x,
+		await Bun.write(join(dst,x.path),x.html),
+		0
+	))
+);
